8bfe82a38b2b3074fb0182f4f474fb68b6f5a04c
zhangkeyi <a479115051@126.com>
2021-05-25 21:49:08 +0800

    add namespace demo

---
+++ namespace.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+namespace foo 
+{
+    int value () 
+    {
+        return 5;
+    }
+}
+
+namespace bar
+{
+    const double pi = 3.1416;
+    double value () {return 2 * pi;}
+}
+
+int main () 
+{
+    std::cout << foo::value() << std::endl;
+    std::cout << bar::value() << std::endl;
+    std::cout << bar::pi << std::endl;
+    return 0;
+}
